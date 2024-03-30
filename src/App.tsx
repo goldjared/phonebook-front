0fea8e150c30e9947642d5a9cc6c75b35b2e8680
jared <jaredgold@proton.me>
2024-03-30 16:15:17 -0400

    add search input, add search state and srch method to filter peros arr

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { useState } from 'react';
 import { Number } from './components/Number';
 
+interface FilteredProperties {
+  name: string;
+  number: string;
+  id: number;
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'John Smith', number: '000-123-4567' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])  
+  const [filteredResults, setFilteredResults] = useState<FilteredProperties[]>([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [newSearch, setNewSearch] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -12,6 +25,7 @@ const App = () => {
     const phoneObj = {
       name: newName,
       number: newNumber,
+      id: 123,
     };
 
     if(persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
@@ -33,10 +47,22 @@ const App = () => {
     console.log(event.target.value);
     setNewNumber(event.target.value);
   };
+
+  const searchItems = (searchValue) => {
+    setNewSearch(searchValue)
+   const filteredData = persons.filter((item) => {
+      return Object.values(item).join('').toLowerCase().includes(newSearch.toLowerCase())
+    }) 
+    setFilteredResults(filteredData);
+  }
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          search: <input value={newSearch} onChange={(e) => searchItems(e.target.value)} />
+        </div>
       <div>debug: {newName}</div>
+      <h2>Add a New</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
