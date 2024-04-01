ea4ad3ee3d070207750df42e1043d34e3343901e
jared <jaredgold@proton.me>
2024-03-31 22:38:47 -0400

    refactor search into own component, format App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Number } from './components/Number';
+import { Search } from './components/Search';
 
 interface PersonEntry {
   name: string;
@@ -12,12 +13,11 @@ const App = () => {
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])  
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [filteredResults, setFilteredResults] = useState<PersonEntry[]>([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
-  const [newSearch, setNewSearch] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -28,16 +28,20 @@ const App = () => {
       id: 123,
     };
 
-    if(persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
-      alert(`${newName} is already added to the phonebook`)
+    if (
+      persons.some(
+        (person) => person.name.toLowerCase() === newName.toLowerCase()
+      )
+    ) {
+      alert(`${newName} is already added to the phonebook`);
     } else {
       setPersons(persons.concat(phoneObj));
     }
 
     setNewName('');
     setNewNumber('');
-    setNewSearch('');
-    setFilteredResults([]);
+    // setNewSearch('');
+    // setFilteredResults([]);
   };
 
   const handleNameChange = (event) => {
@@ -50,23 +54,13 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
-  const searchItems = (searchValue) => {
-    setNewSearch(searchValue)
-      const filteredData = persons.filter((item) => {
-      return Object.values(item).join('').toLowerCase().includes(newSearch.toLowerCase())
-    }) 
-    // resets filtered to empty if the search input is cleared
-    searchValue === "" 
-      ? setFilteredResults([])
-      : setFilteredResults(filteredData);
-    
-  }
   return (
     <div>
       <h2>Phonebook</h2>
-        <div>
-          search: <input value={newSearch} onChange={(e) => searchItems(e.target.value)} />
-        </div>
+      <Search
+        persons={persons}
+        setFilteredResults={setFilteredResults}
+      ></Search>
       <div>debug: {newName}</div>
       <h2>Add a New</h2>
       <form onSubmit={addPerson}>
@@ -82,13 +76,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        { filteredResults.length > 0 ? filteredResults.map((person) => (
-          <Number key={person.name} person={person} />
-          ))
-        : persons.map((person) => (
-          <Number key={person.name} person={person} />
-        ))}
-      
+        {filteredResults.length > 0
+          ? filteredResults.map((person) => (
+              <Number key={person.name} person={person} />
+            ))
+          : persons.map((person) => (
+              <Number key={person.name} person={person} />
+            ))}
       </ul>
     </div>
   );
---
+++ src/components/Search.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+const Search = ({ persons, setFilteredResults }) => {
+  const [newSearch, setNewSearch] = useState('');
+
+  const searchItems = (searchValue) => {
+    setNewSearch(searchValue);
+    const filteredData = persons.filter((item) => {
+      return Object.values(item)
+        .join('')
+        .toLowerCase()
+        .includes(newSearch.toLowerCase());
+    });
+    // resets filtered to empty if the search input is cleared
+    searchValue === ''
+      ? setFilteredResults([])
+      : setFilteredResults(filteredData);
+  };
+  return (
+    <div>
+      search:{' '}
+      <input value={newSearch} onChange={(e) => searchItems(e.target.value)} />
+    </div>
+  );
+};
+
+export { Search };
