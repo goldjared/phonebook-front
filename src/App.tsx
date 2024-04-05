8654eb2c2f5ecb8aa8d9b4dc4eef90ced002bd0c
jared <jaredgold@proton.me>
2024-04-05 00:40:21 -0400

    retrieve people from backend server with axios get

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Search } from './components/Search';
 import { AddForm } from './components/AddForm';
 import { PersonDisplay } from './components/PersonDisplay';
 import type { PersonEntry } from './types/phonebookTypes';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState<PersonEntry[]>([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filteredResults, setFilteredResults] = useState<PersonEntry[]>([]);
+
+  useEffect(() => {
+    console.log('effect');
+    axios.get('http://localhost:3000/notes').then((res) => {
+      console.log('promise fulfilled');
+      setPersons(res.data);
+    });
+  }, []);
+
+  console.log('render', persons.length, 'notes');
 
   return (
     <div>
