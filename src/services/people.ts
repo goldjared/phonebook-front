cf41fd2fb65fba398a96b14b474b194b2a87581d
jared <jaredgold@proton.me>
2024-04-08 14:31:37 -0400

    add remove method, format

--- src/services/people.ts
+++ src/services/people.ts
@@ -1,20 +1,24 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3000/notes'
+import axios from 'axios';
+const baseUrl = 'http://localhost:3000/notes';
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
 
+const remove = (id: number) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
 
-export default { getAll, create, update }
+export default { getAll, create, update, remove };
