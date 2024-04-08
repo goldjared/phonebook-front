25baaf39c19bff71cae3160eedc2a8accab3290b
jared <jaredgold@proton.me>
2024-04-08 15:31:08 -0400

    add delPerson method, update state on deletion

--- src/components/Number.tsx
+++ src/components/Number.tsx
@@ -1,10 +1,20 @@
-const Number = ({ person }) => {
+import peopleService from '../services/people';
+
+const Number = ({ person, persons, setPersons }) => {
+  const delPerson = () => {
+    console.log(person.name);
+    peopleService.remove(person.id).then((res) => {
+      console.log(res.status);
+      // update person state to remove deleted entry
+      setPersons(persons.filter((entry) => entry.id !== person.id));
+    });
+  };
   return (
     <div className='person-entry'>
       <li>
         {person.name} {person.number}
       </li>
-      <button>delete</button>
+      <button onClick={delPerson}>delete</button>
     </div>
   );
 };
