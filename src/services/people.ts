b025c6d631ba46a79e8ba941a9056f0d62fab461
jared <jaredgold@proton.me>
2024-04-08 20:40:57 -0400

    change baseUrl to server

--- src/services/people.ts
+++ src/services/people.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3000/people';
+// const baseUrl = 'http://localhost:3000/people';
+const baseUrl = 'http://001.fly.dev/people';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
