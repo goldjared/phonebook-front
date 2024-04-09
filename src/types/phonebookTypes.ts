642c7986b64f04081ed471584ee3d11dd35a5e8c
jared <jaredgold@proton.me>
2024-04-08 22:51:05 -0400

    create interfaces for prop types

--- src/types/phonebookTypes.ts
+++ src/types/phonebookTypes.ts
@@ -4,4 +4,34 @@ interface PersonEntry {
   id: number;
 }
 
-export type { PersonEntry };
+interface PersonProps {
+  persons: PersonEntry[];
+  setPersons: React.Dispatch<React.SetStateAction<PersonEntry[]>>;
+}
+
+interface FilteredProps {
+  filteredResults: PersonEntry[];
+  setFilteredResults: React.Dispatch<React.SetStateAction<PersonEntry[]>>;
+}
+
+interface SearchProps {
+  setFilteredResults: React.Dispatch<React.SetStateAction<PersonEntry[]>>;
+  persons: PersonEntry[];
+}
+
+interface PersonDisplay extends PersonProps {
+  filteredResults: PersonEntry[];
+}
+
+interface NumberProp extends PersonProps {
+  person: PersonEntry;
+}
+
+export type {
+  PersonEntry,
+  PersonProps,
+  NumberProp,
+  FilteredProps,
+  SearchProps,
+  PersonDisplay,
+};
